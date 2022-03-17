88dad154513f3b459793a89f60cdb104769d5681
warren roque <warrenroque17@gmail.com>
2022-03-17 15:28:16 +0000

    add movement commands

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
+let connection;
 
-
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +14,18 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w' || key === 'W') {
+    connection.write('Move: up');
+  }
+  if (key === 'a' || key === 'A') {
+    connection.write('Move: left');
+  }
+  if (key === 's' || key === 'S') {
+    connection.write('Move: down');
+  }
+  if (key === 'd' || key === 'D') {
+    connection.write('Move: right');
+  }  
 };
 
 module.exports = setupInput;
