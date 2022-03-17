c846b70ba0d55676c2ed9243840d00b0fe1bf8e7
warren roque <warrenroque17@gmail.com>
2022-03-17 07:23:35 +0000

    add movement command

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
+const net = require('net');
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15', // IP address here,
-    port: 50541 // PORT number here,
-  });
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
+console.log('Connecting ...');
 connect();
 
-  // code that does something when the connection is first established
